e0de7cfffb93f7d8bfddb06134607c4aa036a570
hotpoor <liwei.xia0223@gmail.com>
2021-05-28 18:17:22 +0800

    Commit part

---
+++ Application-Challenge/【夏力维队】Web共享编辑页面生成器/code/www/static/js/react_dev/test.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      // return 'You liked this.';
+      return (
+        <button onClick={() => this.setState({ liked: false })}>
+          Un Like
+        </button>
+      );
+
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+
+// const {Button}=MaterialUI
+// function App() {
+//   return (
+//     <Button variant="contained" color="primary">
+//       你好，世界
+//     </Button>
+//   );
+// }
+
+// ReactDOM.render(<App />, document.querySelector('#app'));
+
+const {
+  colors,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  Container,
+  makeStyles,
+  createMuiTheme,
+  Box,
+  SvgIcon,
+  Link,
+} = MaterialUI;
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: colors.red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
+
+function LightBulbIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
+    </SvgIcon>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: theme.spacing(6, 0, 3),
+  },
+  lightBulb: {
+    verticalAlign: 'middle',
+    marginRight: theme.spacing(1),
+  },
+}));
+
+function ProTip() {
+  const classes = useStyles();
+  return (
+    <Typography className={classes.root} color="textSecondary">
+      <LightBulbIcon className={classes.lightBulb} />
+      Pro tip: See more{' '}
+      <Link href="https://material-ui.com/getting-started/templates/">
+        templates
+      </Link>{' '}
+      on the Material-UI documentation.
+    </Typography>
+  );
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <div style={{ marginTop: 24, }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          CDN v4-beta example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </div>
+    </Container>
+  );
+}
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector('#root'),
+);
