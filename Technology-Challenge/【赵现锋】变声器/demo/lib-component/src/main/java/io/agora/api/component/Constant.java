62cc506858256bfc16e100c71acc40c582968adf
ZxfBugProgrammer <1150123839@qq.com>
2021-06-01 22:44:51 +0800

    v1

---
+++ Technology-Challenge/【赵现锋】变声器/demo/lib-component/src/main/java/io/agora/api/component/Constant.java
@@ -0,0 +1,19 @@
+package io.agora.api.component;
+
+import android.view.TextureView;
+
+import io.agora.rtc.RtcEngine;
+
+public class Constant {
+    public static TextureView TEXTUREVIEW;
+
+    public static RtcEngine ENGINE;
+
+    public static String TIPS = "tips";
+
+    public static String DATA = "data";
+
+    public static final String MIX_FILE_PATH = "/assets/music_1.m4a";
+
+    public static final String EFFECT_FILE_PATH = "/assets/effectA.wav";
+}
