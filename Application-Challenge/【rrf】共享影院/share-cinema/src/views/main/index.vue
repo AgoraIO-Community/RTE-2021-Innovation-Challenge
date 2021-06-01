5dc4317f96b9be8b97dd46e974e7de06d23db14a
pharra <typechasing@gmail.com>
2021-06-01 17:33:08 +0800

    remove avatar mini

--- Application-Challenge/【rrf】共享影院/share-cinema/src/views/main/index.vue
+++ Application-Challenge/【rrf】共享影院/share-cinema/src/views/main/index.vue
@@ -25,7 +25,6 @@
             <div class="user">
               <div class="avatar">
                 <el-avatar
-                  :size="mini"
                   src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
                 ></el-avatar>
               </div>
