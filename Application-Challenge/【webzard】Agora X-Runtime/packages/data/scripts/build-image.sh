eee0ce3396bf6e1208518e6974b6b75cf1c9df24
Cafory <32298813+Cafory@users.noreply.github.com>
2021-06-03 02:59:46 +0800

    Delete Application-Challenge/【webzard】Agora X-Runtime directory

--- Application-Challenge/【webzard】Agora X-Runtime/packages/data/scripts/build-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker buildx build --platform linux/amd64 . --push -t yanzhen/x-runtime-data:$1
--- Application-Challenge/【webzard】Agora X-Runtime/packages/send-mail/scripts/build-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker buildx build --platform linux/amd64 . --push -t yanzhen/send-mail:$1
